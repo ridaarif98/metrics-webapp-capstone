c636ec831001a08613c7aea4db0fdf5e213d1b77
ridaarif98 <ridaarif16@gmail.com>
2021-10-11 13:21:39 +0200

    Design home page

--- src/compoenets/display.js
+++ src/compoenets/display.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import '../stylesheets/display.css';
+import image from '../images/europe.png';
 
 const DisplayData = () => {
   const disptach = useDispatch();
   const covidData = useSelector((state) => state.covidReducer);
-  console.log(covidData);
-
   return (
     <div>
+      <div className="HeadingDiv">
+        <img src={image} alt="Europe" />
+        <h1>Europe</h1>
+      </div>
       <ul>
         {covidData.map((data) => (
           <li>
-            {data.confirmed}
             <p>{data.country}</p>
+            <span> {data.confirmed}</span>
           </li>
         ))}
       </ul>
