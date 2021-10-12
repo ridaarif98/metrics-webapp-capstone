f9ed97f891dbe364e8d054774c7bcf24be93b80d
ridaarif98 <ridaarif16@gmail.com>
2021-10-12 21:05:02 +0200

    Fix some linter errors

--- src/redux/country/country.js
+++ src/redux/country/country.js
@@ -1,23 +1,23 @@
 const FETCH_COUNTRY_DATA = 'metric-webapp-capstone/country/FETCH_COUNTRY_DATA';
 const initialState = [];
 export const loadCountryData = (countryValue, countryConfirm) => async (dispatch) => {
-    const covidGet = await fetch(`https://covid-api.mmediagroup.fr/v1/cases/?country=${countryValue}`);
-    const covidList = await covidGet.json();
-    const twons = [countryValue, countryConfirm];
-    for (const key in covidList) {
-      if (key !== 'All') {
-        const newTwon = {
-          name: key,
-          confirmed: covidList[key].confirmed,
-        };
-        twons.push(newTwon);
-      }
+  const covidGet = await fetch(`https://covid-api.mmediagroup.fr/v1/cases/?country=${countryValue}`);
+  const covidList = await covidGet.json();
+  const twons = [countryValue, countryConfirm];
+  for (const key in covidList) {
+    if (key !== 'All') {
+      const newTwon = {
+        name: key,
+        confirmed: covidList[key].confirmed,
+      };
+      twons.push(newTwon);
     }
-    dispatch({
-      type: FETCH_COUNTRY_DATA,
-      payload: twons,
-    });
-  };
+  }
+  dispatch({
+    type: FETCH_COUNTRY_DATA,
+    payload: twons,
+  });
+};
 
 const countryReducer = (state = initialState, action) => {
   switch (action.type) {
